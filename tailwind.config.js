89251cfe2415f75288c44166dc6f5ac7549fda2b
f4h3m <fahemdj99@gmail.com>
2022-11-17 05:52:04 +0600

    🚀 First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit', // Just-In-Time Compiler
+  content: ['./src/**/*.html'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
